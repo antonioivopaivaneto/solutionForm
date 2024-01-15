05bcdaa1c1d6b21705da09b3118ee6dc3b5090e1
junior <antonioivo.3@gmail.com>
2024-01-15 12:21:33 -0300

    initial

--- routes/web.php
+++ routes/web.php
@@ -33,5 +33,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-var_dump
+
 require __DIR__.'/auth.php';
