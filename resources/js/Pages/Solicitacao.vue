b5d06f8ce04dcf3821e8b22a669a1a0f1f05ac5b
junior <antonioivo.3@gmail.com>
2024-01-16 17:08:43 -0300

    validacao

--- resources/js/Pages/Solicitacao.vue
+++ resources/js/Pages/Solicitacao.vue
@@ -2,7 +2,7 @@
     <div
         class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-blue-200 dark:bg-dots-lighter dark:bg-blue-200 selection:bg-red-500 selection:text-white">
         <div class="p-5 pt-10">
-            <div class=""  v-if="!stepTree" >
+            <div class="" v-if="!stepTree">
                 <h1 class="text-4xl text-gray-900 text-blue-900">
                     <b class="text-5xl">Faça</b> <br />Sua Solicitação
                 </h1>
@@ -14,7 +14,7 @@
             <form class="max-w-sm mx-auto mt-6 fade">
 
 
-                <img :src="header"  v-if="!stepTree" class="transition w-80 mx-auto mt-6" alt="">
+                <img :src="header" v-if="!stepTree" class="transition w-80 mx-auto mt-6" alt="">
 
                 <transition>
 
@@ -22,28 +22,19 @@
 
 
                         <div class="mb-5 mt-2">
-                            <vue3-simple-typeahead
-                            v-model="form.assunto"
-
-    class="bg-gray-50 border text-gray-700 border-gray-300 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-    id="assunto"
-    placeholder="Escolha o assunto..."
-    :items="assuntos"
-    :minInputLength="1"
-    @keyup="selectionAssunto()"
-
-  ></vue3-simple-typeahead>
+                            <vue3-simple-typeahead :class="{ 'border-red-600': validacao }" v-model="form.assunto"
+                                class="bg-gray-50 border text-gray-700 border-gray-300 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                id="assunto" placeholder="Escolha o assunto..." :items="assuntos" :minInputLength="1"
+                                @keyup="selectionAssunto()"></vue3-simple-typeahead>
                         </div>
                         <div class="mb-5">
-                            <textarea      @mouseenter="selectionAssunto()"
- v-model="form.solicitacao"
- rows="6" class="bg-gray-50  border-gray-300 text-gray-700 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full
+                            <textarea :class="{ 'border-red-600': validacao }" @mouseenter="selectionAssunto()"
+                                v-model="form.solicitacao" rows="6" class="bg-gray-50  border-gray-300 text-gray-700 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full
         dark:focus:ring-blue-500 dark:focus:border-blue-500" required placeholder="Sua Solicitação"></textarea>
                         </div>
 
                         <div class="mb-5">
-                            <input
-                            @change="uploadFile"
+                            <input @change="uploadFile"
                                 class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none p-2 dark:placeholder-gray-400"
                                 id="file_input" type="file">
 
@@ -57,8 +48,7 @@
 
 
                     <div class="mb-5 mt-2 flex gap-2">
-                        <vue3-simple-typeahead
-                        input="form.condominio"
+                        <vue3-simple-typeahead input="form.condominio" :class="{ 'border-red-600': validacao }"
                             class="bg-gray-50 border text-gray-700  border-gray-300 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             id="condominio" placeholder="Informe seu condominio..." :items="[
                                 'ACACIAS',
@@ -96,27 +86,25 @@
                                 'PRIME LIFE',
                                 'PATIO LUSITANIA'
                             ]
-                                " :minInputLength="1"
-                            @selectItem="alert('teste')"
-                            @keyup="selectionCondominio()"
-                            >
+                                " :minInputLength="1" @selectItem="alert('teste')" @keyup="selectionCondominio()">
                         </vue3-simple-typeahead>
 
-                        <input   @mouseenter="selectionCondominio()  "                    v-model="form.unidade"
- type="text"  placeholder="Sua Unidade"
+                        <input @mouseenter="selectionCondominio()" v-model="form.unidade" type="text"
+                            placeholder="Sua Unidade" :class="{ 'border-red-600': validacao }"
                             class="bg-gray-50 border text-gray-700  border-gray-300 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     </div>
                     <div class="mb-5 ">
-                        <input type="text"  v-model="form.nome"
- placeholder="Seu Nome "
+                        <input type="text" v-model="form.nome" :class="{ 'border-red-600': validacao }"
+                            placeholder="Seu Nome "
                             class="bg-gray-50 border text-gray-700  border-gray-300 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500">
 
                     </div>
                     <div class="mb-5">
-                        <input  v-model="form.email"
- type="email" placeholder="Seu Email "
+                        <input v-model="form.email" :class="{ 'border-red-600': validacaoEmail }" type="email"
+                            placeholder="Seu Email " required
                             class="bg-gray-50 border text-gray-700  border-gray-300 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500">
 
+                            <span class="text-red-500 mx-auto text-center text-xs">{{ error }}</span>
                     </div>
 
                 </div>
@@ -128,8 +116,8 @@
                     </h1>
 
                     <p class="text-gray-700 font-thin  mb-6">
-                Sua solicitação sera enviada diretamente para a sindicancia. Gestão Eficiênte e Participativa
-            </p>
+                        Sua solicitação sera enviada diretamente para a sindicancia. Gestão Eficiênte e Participativa
+                    </p>
                 </div>
 
                 <div class="flex flex-row-reverse gap-10">
@@ -137,7 +125,7 @@
                         class="text-white mb-6  bg-green-900 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
                         finalizar
                     </button>
-                    <button type="button" v-if="stepOne" @click="ShowStep2()"
+                    <button :class="{ 'cursor-not-allowed': !validacao }" type="button" v-if="stepOne" @click="ShowStep2()"
                         class="text-white mb-6  bg-blue-900 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Proximo
                     </button>
@@ -160,20 +148,30 @@ import gif from "./../../img/success.gif";
 import { reactive, ref } from "vue";
 import { router } from "@inertiajs/vue3";
 
-const  assuntos =  [
-        'Reclamação',
-        'Sugestão',
-        'Elogio',
-        'Hall',
-        'Estacionamento',
-        'Portaria',
-        'Limpeza',
-        'Manutenção',
-        'Segurança',
-        'Áreas Comuns',
-        'Reservas',
-      ]
+const isEmailValid = (email) =>{
 
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+
+
+}
+
+const assuntos = [
+    'Reclamação',
+    'Sugestão',
+    'Elogio',
+    'Hall',
+    'Estacionamento',
+    'Portaria',
+    'Limpeza',
+    'Manutenção',
+    'Segurança',
+    'Áreas Comuns',
+    'Reservas',
+]
+const error = ref()
+const validacaoEmail = ref(false);
+const validacao = ref(false);
 const finalizar = ref(false)
 const stepTwo = ref(false)
 const stepTree = ref(false)
@@ -181,34 +179,41 @@ const stepOne = ref(true)
 const Npasso = ref(1)
 
 const form = reactive({
-  assunto: null,
-  solicitacao: null,
-  foto: null,
-  condominio: document.getElementById('condominio'),
-  unidade: null,
-  nome: null,
-  email: null,
+    assunto: null,
+    solicitacao: null,
+    foto: null,
+    condominio: document.getElementById('condominio'),
+    unidade: null,
+    nome: null,
+    email: null,
 })
 
 
 const uploadFile = (event) => {
-        form.foto= event.target.files[0];
+    form.foto = event.target.files[0];
 }
 
 const selectionAssunto = () => {
 
-    form.assunto= document.getElementById('assunto').value
+    form.assunto = document.getElementById('assunto').value
 
 }
 const selectionCondominio = () => {
 
-    form.condominio= document.getElementById('condominio').value
+    form.condominio = document.getElementById('condominio').value
 
 }
 
 
 
 const ShowStep2 = () => {
+
+    if (!form.assunto || !form.solicitacao) {
+        validacao.value = true
+        return
+    }
+    validacao.value = false
+
     stepTree.value = false
     stepOne.value = false
     stepTwo.value = true
@@ -216,17 +221,32 @@ const ShowStep2 = () => {
     finalizar.value = true
 }
 const ShowStep1 = () => {
+
     stepOne.value = true
     stepTwo.value = false
     stepTree.value = false
     Npasso.value = 1
     finalizar.value = false
 
-    console.log(document.getElementById('assunto_typeahead').value())
-
 }
 
 const ShowStep3 = () => {
+
+    if (!form.assunto || !form.solicitacao || !form.condominio || !form.unidade || !form.nome ) {
+        validacao.value = true
+        return
+    }
+
+    if(!isEmailValid(form.email)){
+        validacaoEmail.value = true
+
+        error.value = "email Invalido"
+        return
+
+    }
+    validacaoEmail.value = false
+    validacao.value = false
+    error.value = ""
 
     router.post('/solicitacao', form)
 
